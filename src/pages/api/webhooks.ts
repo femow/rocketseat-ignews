d6ed6a923d4fa8566616dbf2a6474406fad90c46
femow <felipemu_92@hotmail.com>
2022-06-30 20:05:54 -0300

    Finishing subscription events

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,5 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { Readable } from 'stream'
+import Stripe from 'stripe';
+import { stripe } from '../../services/stripe';
+import { saveSubscription } from './_lib/manageSubscription';
 
 async function buffer(readable: Readable) {
     const chunks = []
@@ -19,11 +22,58 @@ export const config = {
     }
 }
 
+const relevantEvent = new Set([
+    'checkout.session.completed',
+    'customer.subscriptions.updated',
+    'customer.subscriptions.deleted',
+])
+
 export default async function(request: NextApiRequest, response: NextApiResponse) {
     if (request.method === 'POST') {
         const buf = await buffer(request)
+        const secret = request.headers['stripe-signature']
+
+        let event: Stripe.Event
+
+        try {
+            event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
+        } catch (err) {
+            return response.status(400).send('Webhook error: ' + err.message)
+        }
+
+        const type = event.type
+
+        if (relevantEvent.has(type)) {
+
+            try {
+                switch (type) {
+                    case 'customer.subscriptions.updated':
+                    case 'customer.subscriptions.deleted':
+                        const subscription = event.data.object as Stripe.Subscription
+
+                        await saveSubscription(
+                            subscription.id,
+                            subscription.customer.toString(),
+                            false
+                        )
+                        break;
+                    case 'checkout.session.completed':
+                        const checkoutSession = event.data.object as Stripe.Checkout.Session
+                        await saveSubscription(
+                            checkoutSession.subscription.toString(),
+                            checkoutSession.customer.toString(),
+                            true
+                        )
+                        break;
+                    default:
+                        throw new Error('Unhandle event.')
+                }
+            } catch (err) {
+                return response.json({ error: 'Webhook handler failed.' })
+            }
+        }
         
-        response.status(200).json({ ok: true })
+        response.json({ received: true })
     } else {
         response.setHeader('Allow', 'POST')
         response.status(405).end('Method not allowed')
